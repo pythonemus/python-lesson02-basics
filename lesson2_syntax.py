d2184fed3cd051ce3efc6258552ad28572dca03a
pythonemus <n.dambrosio67@gmail.com>
2022-09-21 05:35:10 +0000

    add lesson2 syntax

---
+++ lesson2_syntax.py
@@ -0,0 +1,35 @@
+#Execute Python Syntax
+########################
+
+#We can execute python code by either typing directly into 
+#python CLI
+
+#Example
+# >>> print("Hello, World!")
+# Hello, World!
+
+#Or execute python code from a file
+#Example
+#C:\Users\Your Name>python myfile.py
+
+
+#Python Indentation
+######################
+
+#Indentation refers to the spaces at the beginning of a code line.
+#Python uses indentation to indicate a block of code.
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+#The number of spaces is up to you as a programmer
+#The most common use is four, 
+#But it has to be at least one.
+
+if 6 > 3:
+  print("six is greater than three!")
+if 6 > 3:
+    print("six is greater than three!")
+
+#You must have the same number of spaces in the same block of code
+#otherwise Python will give you an error:
