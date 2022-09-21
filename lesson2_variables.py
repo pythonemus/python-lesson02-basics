7d85b8f7e1a2c7e63352bd416bb2f2f68ada7176
pythonemus <n.dambrosio67@gmail.com>
2022-09-21 05:19:56 +0000

    add lesson2 variables

---
+++ lesson2_variables.py
@@ -0,0 +1,34 @@
+#Creating A Varible
+##############
+"""
+Python has no command for declaring a variable.
+A variable is created the moment you first assign a value to it.
+"""
+
+
+#String - length of char between quotes
+#makes a string
+string = "thing is a string"
+print(string)
+
+#Integer - numbers and decimal
+integer = 100
+print(100)
+
+#boolean - is a varibale that is
+#either set to True or False
+boolean_true = True
+boolean_false = False
+print(boolean_true)
+print(boolean_false)
+
+#Casting on Variables
+#######################
+
+#If you want to specify the data type of a variable.
+#This can be done using the casting methods native to python.
+
+strg = str(10)
+intgr = int(10)
+print(type(strg))
+print(type(intgr))
