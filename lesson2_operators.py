fd4636f49947bf78ea1304fd4066afd60e75c8ab
pythonemus <n.dambrosio67@gmail.com>
2022-09-21 06:15:59 +0000

    add operators

---
+++ lesson2_operators.py
@@ -0,0 +1,26 @@
+#Python Operators
+###################
+
+#Operators are used to perform operations on variables and values.
+
+'''
+
+Python divides the operators in the following groups:
+- Arithmetic operators
+- Assignment operators
+- Comparison operators
+-Logical operators
+- Identity operators
+- Membership operators
+- Bitwise operators
+
+'''
+x = 5
+print(10 + 5)   #Arithmetic Operator
+print(x)    #Assignment Operator
+print(x == 5)   #Comparison Operator
+
+if (x > 1) and (x > 2):
+    print("Logical Operators")
+
+print(x is 5) #Identity Operator
